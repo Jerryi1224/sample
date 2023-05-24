39e00983d212fc38910e39f05a1677e9f14e0463
Jerryi1224 <35223411+Jerryi1224@users.noreply.github.com>
2023-05-24 21:17:19 +0800

    affix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+use std::sync::Mutex;
+
+use salvo::affix;
+use salvo::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
+    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    Server::new(acceptor).serve(route()).await;
+}
+
+#[allow(dead_code)]
+#[derive(Default, Clone, Debug)]
+struct Config {
+    username: String,
+    password: String,
+}
+
+#[derive(Default, Debug)]
+struct State {
+    fails: Mutex<Vec<String>>,
+}
+
+#[handler]
+async fn hello(depot: &mut Depot) -> String {
+    let config = depot.obtain::<Config>().unwrap();
+    let custom_data = depot.get::<&str>("custom_data").unwrap();
+    let state = depot.obtain::<Arc<State>>().unwrap();
+    let mut fails_ref = state.fails.lock().unwrap();
+    fails_ref.push("fail message".into());
+    format!("Hello world\nConfig: {config:#?}\nFails: {fails_ref:#?}\nCustom Data: {custom_data}")
+}
+
+fn route() -> Router {
+    let config = Config {
+        username: "root".to_string(),
+        password: "pwd".to_string(),
+    };
+    Router::new()
+        .hoop(
+            affix::inject(config)
+                .inject(Arc::new(State {
+                    fails: Mutex::new(Vec::new())
+                }))
+                .insert("custom_data", "I Love this world!")
+        )
+        .get(hello)
+        .push(
+            Router::with_path("hello")
+        )
+        .get(hello)
+
 }
